3cad0a500486ef703acb78d17f4a395a2135b776
obvSamuray <dava.safarov@gmail.com>
2023-04-15 18:33:57 +0300

    0.2

---
+++ graphclass.py
@@ -0,0 +1,32 @@
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+
+    def construct_graph(self, nodes, init_graph):
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+
+        graph.update(init_graph)
+
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+
+        return graph
+
+    def get_nodes(self):
+        return self.nodes
+
+    def get_outgoing_edges(self, node):
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+
+    def value(self, node1, node2):
+        return self.graph[node1][node2]
+
